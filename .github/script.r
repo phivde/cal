4bbca7e9177fdc35053c2978094d94c9a22b1be2
phivde <phivde@gmail.com>
2026-07-22 19:14:05 +0200

    Refactor calendar data processing in script.r
    Updated the script to include common columns and modified the way data is combined from two calendar sources.

--- .github/script.r
+++ .github/script.r
@@ -1,14 +1,17 @@
-# script.R
-# Installer/charger les packages requis
-if (!require("calendar")) install.packages("calendar")
 library(calendar)
+# library(tidyverse)
 # lecture M1 CEPE
 ics_M1 = ic_read("https://calendar.google.com/calendar/ical/mdjlce93a22mdeq2njgmgjujto%40group.calendar.google.com/public/basic.ics")
 head(ics_M1)
 # lecture M2 CEPE
 ics_M2 = ic_read("https://calendar.google.com/calendar/ical/0h4ucbt6c4ermrtaehgidndbdk%40group.calendar.google.com/public/basic.ics")
 head(ics_M2)
-Fusion <- result <- bind_rows(ics_M1,ics_M2)
+# Fusion <- result <- bind_rows(ics_M1,ics_M2)
+
+common <- intersect(colnames(ics_M1), colnames(ics_M2))
+Fusion <- rbind(ics_M1[common],ics_M2[common])
+# Extrait <- Fusion %>%
+#  filter(grepl('EECKAUT',SUMMARY)) 
 Extrait <- subset(Fusion,grepl('EECKAUT',SUMMARY)) 
 head(Extrait)
 
