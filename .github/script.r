f80a09e39c9e41766e0db5745a9504456d47bc21
phivde <phivde@gmail.com>
2026-07-23 17:10:58 +0200

    Update script.r

--- .github/script.r
+++ .github/script.r
@@ -16,6 +16,6 @@ Extrait <- subset(Fusion,grepl('EECKAUT',SUMMARY))
 head(Extrait)
 
 # Ecriture du fichier filtré
-ic_write(ical(Extrait),  "Extrait.ics",zulu=2)
+ic_write(ical(Extrait),  "Extrait.ics",zulu=TRUE)
 # Ecriture du fichier global
 # ic_write(ical(Fusion),  "CEPE.ics")
